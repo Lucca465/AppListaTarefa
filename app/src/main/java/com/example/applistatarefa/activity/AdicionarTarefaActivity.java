2f005b1bb0b35ab2908ea5949577de6afbe85f7d
Lucca465 <lucca465@gmail.com>
2021-10-28 19:18:31 -0300

    Adicionado o menu

--- app/src/main/java/com/example/applistatarefa/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/com/example/applistatarefa/activity/AdicionarTarefaActivity.java
@@ -3,6 +3,9 @@ package com.example.applistatarefa.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.applistatarefa.R;
 
@@ -12,5 +15,25 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.itemSalvar:
+                // codigo
+                Toast.makeText(AdicionarTarefaActivity.this,
+                        "Botão salvar pressionado",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
